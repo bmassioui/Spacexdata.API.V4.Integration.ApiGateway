0964f5e64c3ae3804a7168f29a63dbd9cc5b0553
bmassioui <bouchaib.massioui@gmail.com>
2023-05-25 00:25:19 +0100

    :package: Initialize the Api controller with empty endpoints

---
+++ src/ApiGateway.WebApi/Controllers/LaunchesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.WebApi.Controllers;
+
+public class LaunchesController : ApiControllerBase
+{
+    /// <summary>
+    /// Past launches
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet(Name = "/", Order = 0)]
+    public IActionResult GetPastLaunches()
+    {
+        return Ok();
+    }
+
+    /// <summary>
+    /// Past launch by id
+    /// </summary>
+    /// <returns></returns>
+    //[HttpGet(Name = "/{id:string}", Order = 1)]
+    //public IActionResult GetPastLaunchById()
+    //{
+    //    return Ok();
+    //}
+
+    /// <summary>
+    /// Upcoming launches
+    /// </summary>
+    /// <returns></returns>
+    //[HttpGet(Name = "/Upcoming", Order = 2)]
+    //public IActionResult GetUpcomingLaunches()
+    //{
+    //    return Ok();
+    //}
+
+    /// <summary>
+    /// Upcoming launch by id
+    /// </summary>
+    /// <returns></returns>
+    //[HttpGet(Name = "/Upcoming/{id:string}", Order = 3)]
+    //public IActionResult GetUpcomingLaunchesById()
+    //{
+    //    return Ok();
+    //}
+}
