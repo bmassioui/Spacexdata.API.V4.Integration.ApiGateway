1fd250919beba2247870fc60c7bc4589d8690612
Bouchaib MASSIOUI <69347197+bmassioui@users.noreply.github.com>
2023-06-01 23:37:32 +0100

    Merge develop into main (#16) (#21)
    * :pencil: Add postman files for backend test
    
    * :fire: update the materials path
    
    * Feature: Add api exception filter to api
    
    * :package: Define Query and Handler for get past launches
    
    * :package: define pagination parameters with documentation
    
    * :package: Test Get past launches
    
    * :package: Separate Models
    
    * :package: Use AutoMapper to map response to application dto

---
+++ src/ApiGateway.Infrastructure/ExternalResources/Models/GetPastLaunchesRequestModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiGateway.Infrastructure.ExternalResources.Models;
+
+public sealed record GetPastLaunchesRequestModel
+{
+    public Options Options { get; set; } = null!;
+}
+
+public record Options
+{
+    public int Offset { get; set; }
+    public int Limit { get; set; }
+    public Sort? Sort { get; set; }
+    public string[] Select { get; set; } = null!;
+}
+
+public record Sort
+{
+    public string Date_Utc { get; set; } = null!;
+}
