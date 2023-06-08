224e4a07ba93e615ab941e170a43c3acd796d84f
bmassioui <bouchaib.massioui@gmail.com>
2023-06-08 18:50:27 +0100

    Unit testing: GetUpcomingLaunches

--- tests/UnitTests/ApiGateway.Application.UnitTests/Features/Launches/Past/Queries/GetUpcomingLaunchesWithPaginationQueryUnitTests.cs
+++ tests/UnitTests/ApiGateway.Application.UnitTests/Features/Launches/Past/Queries/GetUpcomingLaunchesWithPaginationQueryUnitTests.cs
@@ -1,9 +1,40 @@
-﻿namespace ApiGateway.Application.UnitTests.Features.Launches.Past.Queries;
+﻿using ApiGateway.Application.Features.Launches.Upcoming.Queries.GetUpcomingLaunchesWithPagination;
+
+namespace ApiGateway.Application.UnitTests.Features.Launches.Past.Queries;
 
 public class GetUpcomingLaunchesWithPaginationQueryUnitTests
 {
+    private readonly Mock<ILaunchesService> _launchServiceMock;
+    private readonly GetUpcomingLaunchesWithPaginationQueryHandler _getUpcomingLaunchesWithPaginationQueryHandler;
+
     public GetUpcomingLaunchesWithPaginationQueryUnitTests()
     {
-        
+        // Initialization
+        _launchServiceMock = new Mock<ILaunchesService>();
+        _getUpcomingLaunchesWithPaginationQueryHandler = new GetUpcomingLaunchesWithPaginationQueryHandler(_launchServiceMock.Object);
+
+        // Setup
+    }
+
+    [Fact]
+    public async Task HandleShouldReturnDefaultPastLaunchesDtoWhenGetPastLaunchesAsyncReturnsNull()
+    {
+        // Arrange
+        UpcomingLaunchesDto? upcomingLaunchesDto = default;
+        GetUpcomingLaunchesWithPaginationQuery getUpcomingLaunchesWithPaginationQuery = new(default, default);
+        CancellationToken cancellationToken = new();
+
+        _launchServiceMock
+            .Setup(service => service.GetUpcomingLaunchesAsync(It.IsAny<ushort>(), It.IsAny<ushort>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(upcomingLaunchesDto);
+
+        UpcomingLaunchesDto expectedResult = new();
+
+        // Act
+        UpcomingLaunchesDto? result = await _getUpcomingLaunchesWithPaginationQueryHandler.Handle(getUpcomingLaunchesWithPaginationQuery, cancellationToken);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.True(result == expectedResult);
     }
 }
